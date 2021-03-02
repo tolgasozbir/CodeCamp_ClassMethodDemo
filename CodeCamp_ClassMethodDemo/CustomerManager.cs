d32605f36867786492623eba4bed8bd0ba7cf595
Tolga SÖZBİR <tolgasoz1@gmail.com>
2021-03-03 00:33:51 +0300

    .

---
+++ CodeCamp_ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeCamp_ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer Added \nId : " + customer.Id + "\nName : " + customer.Name + "\nSurname : " + customer.Surname);
+            Console.WriteLine("----------");
+        }
+
+        public void CustomerList(Customer[] customer)
+        {
+            Console.WriteLine("Customer List");
+            foreach (var item in customer)
+            {
+                Console.WriteLine(item.Id + " " + item.Name + " " + item.Surname);
+            }
+
+        }
+    }
+}
--- CodeCamp_ClassMethodDemo/Program.cs
+++ CodeCamp_ClassMethodDemo/Program.cs
@@ -6,7 +6,26 @@ namespace CodeCamp_ClassMethodDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Tolga";
+            customer1.Surname = "Sözbir";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Levent";
+            customer2.Surname = "Sözbir";
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.AddCustomer(customer1);
+            customerManager.AddCustomer(customer2);
+
+            customerManager.CustomerList(customers);
+
+            Console.ReadKey();
         }
     }
 }
